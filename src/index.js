ee5d9811737ba8014e81ef69b7dea76afe59ad0e
Fernanda Vidal <ifervb@gmail.com>
2023-02-13 13:10:37 -0600

    GET: input value

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import validator from './validator.js';
+import validator from "./validator.js";
+
+// traer el nunmero del DOM
+
+const btn = document.querySelector(".btn"),
+  inputNumber = document.querySelector(".numberCard");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // obtener el valor del input
+  const numberValue = inputNumber.value;
+
+  //   validar: solo numeros 0-9, solo 16 digitos, no campos vacios
+  const regEx = /^[0-9]/;
+
+  const validacion = regEx.test(numberValue);
+
+  console.log(validacion)
+//   if (regEx.search(numberValue)) {
+//     console.log("tiene letras");
+//   } else {
+//     console.log("Formato correcto");
+//   }
+
+  //   ejecutar la funcion isValid e isMaskify
+});
 
 console.log(validator);
