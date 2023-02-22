717334c479306df7b0f7bd52c55292cfb1e44847
Fernanda Vidal <ifervb@gmail.com>
2023-02-21 19:43:59 -0600

    GET: informaciòn sobre la tarjeta

--- src/index.js
+++ src/index.js
@@ -4,18 +4,28 @@ import validator from "./validator.js";
 
 const btn = document.querySelector(".btn"),
   inputNumber = document.querySelector(".numberCard"),
+  inputName = document.querySelector(".nameCard"),
+  name = document.querySelector(".name"),
+  inputDate = document.querySelector(".dateCard"),
+  date = document.querySelector(".date"),
+  inputCvv = document.querySelector(".cvvCard"),
+  cvv = document.querySelector(".cvv"),
   validText = document.querySelector(".validText"),
   errorText = document.querySelector(".errorText"),
   cardNumber = document.querySelector(".card__info-number");
 
-document.addEventListener("keypress", (e) => {
+inputNumber.addEventListener("keypress", (e) => {
   e.charCode >= 48 && e.charCode <= 57 ? true : e.preventDefault();
 });
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  // obtener el valor del input
-  const numberValue = inputNumber.value;
+  // obtener el valor de los input
+  const numberValue = inputNumber.value,
+    nameValue = inputName.value,
+    dateValue = inputDate.value,
+    cvvValue = inputCvv.value;
+    
   //   validar si solo son numeros
   if (validator.isValid(numberValue) !== false) {
     //   ejecutar la funcion isValid e isMaskify
@@ -23,6 +33,10 @@ btn.addEventListener("click", (e) => {
     errorText.style.display = "none";
     const numMaskify = validator.maskify(numberValue);
     cardNumber.innerHTML = `${numMaskify}`;
+    name.innerHTML = `${nameValue}`;
+    date.innerHTML = `${dateValue}`;
+    cvv.innerHTML = `${cvvValue}`;
+    console.log(dateValue);
   } else {
     validText.style.display = "none";
     errorText.style.display = "block";
