173c38d22123f27397195dd12780b540adf15f62
Fernanda Vidal <ifervb@gmail.com>
2023-02-22 19:48:48 -0600

    ADD: bloque de datos de tarjeta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import validator from "./validator.js";
 
 // traer el nunmero del DOM
 
-const btn = document.querySelector(".btn"),
+const formContainer = document.querySelector(".card__data-container"),
+  btn = document.querySelector(".btn"),
+  btnSave = document.querySelector(".btnSave"),
   inputNumber = document.querySelector(".numberCard"),
   inputName = document.querySelector(".nameCard"),
   name = document.querySelector(".name"),
@@ -17,30 +19,39 @@ const btn = document.querySelector(".btn"),
 inputNumber.addEventListener("keypress", (e) => {
   e.charCode >= 48 && e.charCode <= 57 ? true : e.preventDefault();
 });
+
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-
   // obtener el valor de los input
-  const numberValue = inputNumber.value,
-    nameValue = inputName.value,
-    dateValue = inputDate.value,
-    cvvValue = inputCvv.value;
+  const numberValue = inputNumber.value;
 
   //   validar si solo son numeros
   if (validator.isValid(numberValue) !== false) {
+    const numMaskify = validator.maskify(numberValue);
     //   ejecutar la funcion isValid e isMaskify
     validText.style.display = "block";
     errorText.style.display = "none";
-    const numMaskify = validator.maskify(numberValue);
+    formContainer.style.display = "block";
+
+    inputNumber.value = numMaskify;
+    inputNumber.disabled = true;
+    btn.style.display = "none";
+    btnSave.style.display = "block";
     cardNumber.innerHTML = `${numMaskify}`;
-    name.innerHTML = `${nameValue}`;
-    date.innerHTML = `${dateValue}`;
-    cvv.innerHTML = `${cvvValue}`;
-    console.log(dateValue);
+
+    btnSave.addEventListener("click", (e) => {
+      e.preventDefault();
+      const nameValue = inputName.value,
+        dateValue = inputDate.value,
+        cvvValue = inputCvv.value;
+      name.innerHTML = `${nameValue}`;
+      date.innerHTML = `${dateValue}`;
+      cvv.innerHTML = `${cvvValue}`;
+    });
   } else {
     validText.style.display = "none";
     errorText.style.display = "block";
-    cardNumber.innerHTML = `################`;
+    cardNumber.innerHTML = ``;
     name.innerHTML = ``;
     date.innerHTML = ``;
     cvv.innerHTML = ``;
